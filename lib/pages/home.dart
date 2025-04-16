82da20c6d75a49dbbfe5b982a59085e9e86245e7
NunzioDA <nunziodamore@gmail.com>
2025-04-17 00:38:49 +0200

    Improved UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:iot_park_app/UIUtilities/park_widget.dart';
+import 'package:iot_park_app/UIUtilities/palette.dart';
+import 'package:iot_park_app/communication.dart';
+import 'package:iot_park_app/widgets/ambient_info_box.dart';
+import 'package:iot_park_app/widgets/lights_color_picker_button.dart';
+import 'package:iot_park_app/widgets/park_widget.dart';
 
 class Home extends StatelessWidget{
   const Home({super.key});
@@ -8,108 +12,172 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Palette.backgroundColor,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(40),
+          padding: const EdgeInsets.only(top: 35, left: 50, right: 50, bottom: 50),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               // Top bar
-              SizedBox(
-                height: 45,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Image.asset(
-                      "assets/icons/your-location.png",
-                      height: 35,
-                      width: 35,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text(
-                          "Location",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        Text(
-                          "Urbino, Piazza della Repubblica, 13",
-                          style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                            fontSize: 15,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
+              Column(           
+                crossAxisAlignment: CrossAxisAlignment.center,         
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.black,
+                        size: 20
+                      ),
+                      const Gap(10),
+                      Text(
+                        "Urbino, PU",
+                        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.2
+                        ),                    
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Piazza della Repubblica, 13",
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w100,
+                      letterSpacing: 1.2
+                    ),                                
+                  ),
+                ],
               ),
-              const Gap(30),
+              const Gap(25),
+              AmbientInfoBox(),
+              const Gap(25),
               LayoutBuilder(
                 builder: (context, costraints) {
-
-                  int spacing = 10;
-
-                  double parkWidth = (costraints.maxWidth - (spacing * 2)) / 3;
-
+              
+                  int spacing = 0;
+              
+                  double parkWidth = (costraints.maxWidth - (spacing * 2) - 20) / 3;
+              
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Second floor",),
-                      const Gap(10),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ParkWidget(
-                            width: parkWidth,
-                            type: ParkType.yellow,
-                          ),
-                          ParkWidget(
-                            width: parkWidth,
-                            type: ParkType.grey,
-                            occupied: false,
-                          ),
-                          ParkWidget(
-                            width: parkWidth,
-                            type: ParkType.yellow,
-                          ),
-                        ],
+                      Text(
+                        "Parking Lots",
+                        style: Theme.of(context).textTheme.titleMedium,
                       ),
                       const Gap(20),
-                      Text("First floor",),
-                      const Gap(10),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ParkWidget(
-                            width: parkWidth,
-                            type: ParkType.grey,
-                          ),
-                          ParkWidget(
-                            width: parkWidth,
-                            type: ParkType.yellow,
-                          ),
-                          ParkWidget(
-                            width: parkWidth,
-                            type: ParkType.grey,
-                          ),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5, right: 5),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ParkWidget(
+                              width: parkWidth,
+                              type: ParkType.grey,
+                              occupied: true,
+                              parkId: "A1"
+                            ),
+                            ParkWidget(
+                              width: parkWidth,
+                              type: ParkType.grey,
+                              occupied: true,
+                              parkId: "A2"
+                            ),
+                            ParkWidget(
+                              width: parkWidth,
+                              type: ParkType.grey,
+                              parkId: "A3",
+                              rightBorder: true,
+                            ),
+                          ],
+                        ),
                       ),
-                  
+                      const Gap(30),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5, right: 5),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ParkWidget(
+                              width: parkWidth,
+                              type: ParkType.grey,
+                              parkId: "B1"
+                            ),
+                            ParkWidget(
+                              width: parkWidth,
+                              type: ParkType.grey,
+                              occupied: true,
+                              parkId: "B2"
+                            ),
+                            ParkWidget(
+                              width: parkWidth,
+                              type: ParkType.grey,
+                              parkId: "B3",
+                              rightBorder: true,
+                            ),
+                          ],
+                        ),
+                      ),                      
                     ],
                   );
                 }
               ),
-              
+              const Gap(30),
+              Text(
+                "Lights",
+                style: Theme.of(context).textTheme.titleMedium
+              ),
+              const Gap(5),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  children: [
+                    LightsColorPickerButton(
+                      onColorSelected: (color) {
+                        SmartPark.lightsColor(color);
+                      },
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: SmartPark.lightsOn, 
+                            child: Text("On")
+                          ),
+                        ),
+                        const Gap(10),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: SmartPark.lightsOff, 
+                            child: Text("Off")
+                          ),
+                        ),
+                        const Gap(10),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: SmartPark.lightsAuto, 
+                            child: Text("Auto")
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
-
 }
