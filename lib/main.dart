83c7f8c7b6a89474605ca32aeb37438f1e33d5a0
NunzioDA <nunziodamore@gmail.com>
2025-04-17 00:38:18 +0200

    Improved UI

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextTheme textTheme = GoogleFonts.outfitTextTheme();
+    TextTheme textTheme = GoogleFonts.urbanistTextTheme();
     
     return MaterialApp(
       theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Palette.primary,
+            foregroundColor: Palette.onPrimaryText,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+        colorScheme: ColorScheme.of(context).copyWith(
+          primary: Palette.primary, 
+          onPrimary: Palette.onPrimaryText, 
+          secondary: Palette.secondary,
+        ),
+        primaryColor: Palette.primary,
         textTheme: textTheme.copyWith(
               headlineLarge: textTheme.headlineLarge?.copyWith(
                   fontSize: 35,
                   fontWeight: FontWeight.w900,
-                  color: Palette.primary),
+                  color: Palette.headLine),
               headlineMedium: textTheme.headlineMedium?.copyWith(
                   fontSize: 30,
                   fontWeight: FontWeight.w800,
-                  color: Palette.primary.shade800),
+                  color: Palette.headLine),
               headlineSmall: textTheme.headlineSmall?.copyWith(
                   fontSize: 25,
                   fontWeight: FontWeight.w100,
-                  color: Palette.primary.shade800),
+                  color: Palette.headLine),
               titleLarge: textTheme.titleLarge?.copyWith(
                   fontSize: 25,
                   fontWeight: FontWeight.w800,
-                  color: Palette.primary),
+                  color: Palette.headLine),
               titleMedium: textTheme.titleMedium?.copyWith(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: Palette.primary.shade800),
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 1.2,
+                  color: Colors.black
+                ),
               titleSmall: textTheme.titleSmall?.copyWith(
                   fontSize: 15,
                   fontWeight: FontWeight.w500,
-                  color: Palette.primary.shade800),
+                  color: Palette.headLine),
               bodyMedium: textTheme.bodySmall?.copyWith(
                   fontSize: 15,
                   fontWeight: FontWeight.w200,
-                  color: Colors.grey),
+                  color: Palette.text),
               labelLarge: textTheme.bodyMedium?.copyWith(
                   fontSize: 20,
                   fontWeight: FontWeight.w100,
-                  color: Colors.grey),
+                  color: Palette.headLine),
             ),
       ),
       home: Home(),
