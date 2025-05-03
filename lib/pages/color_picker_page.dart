015d25d65af20f882f7280ddeb988248bec71d89
NunzioDA <nunziodamore99@gmail.com>
2025-05-03 22:55:08 +0200

    added "white" button

--- lib/pages/color_picker_page.dart
+++ lib/pages/color_picker_page.dart
@@ -30,74 +30,100 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
     return Scaffold(
       backgroundColor: Colors.black.withAlpha(100),
       body: SafeArea(
-        child: Center(
-          child: Container(
-            height: ringSize + 70,
-            width: ringSize,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(300),
-                topRight: Radius.circular(300),
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30), 
-              ),
+        child: Stack(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              behavior: HitTestBehavior.opaque,
+              child: Container(),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: Stack(
-                      children: [
-                        SizedBox(
-                          height: ringSize,
-                          width: ringSize,
-                          child: ColorPickerHueRing(
-                            color,
-                            (newColor)=> setState(() {
-                              color = newColor;
-                            }),
-                            strokeWidth: 20,                    
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(50),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: color.toColor(),                            
+            Center(
+              child: Container(
+                height: ringSize + 115,
+                width: ringSize,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(300),
+                    topRight: Radius.circular(300),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30), 
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            SizedBox(
+                              height: ringSize,
+                              width: ringSize,
+                              child: ColorPickerHueRing(
+                                color,
+                                (newColor)=> setState(() {
+                                  newColor = newColor.withSaturation(1);
+                                  color = newColor;
+                                }),
+                                strokeWidth: 20,                    
+                              ),
                             ),
-                          ),
+                            Padding(
+                              padding: const EdgeInsets.all(50),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: color.toColor(),                            
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const Gap(30),
+                      SizedBox(
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(Colors.white),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("White"),
+                              const Gap(5),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(3),
+                                ),
+                                width: 20,
+                                height: 20,
+                              )
+                            ],
+                          )
                         )
-                      ],
-                    ),
+                      ),
+                      const Gap(5),
+                      SizedBox(
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(color.toColor()),
+                          child: Text("Got it!")
+                        )
+                      ),
+                    ],
                   ),
-                  const Gap(30),
-                  SizedBox(
-                    height: 40,
-                    child: ElevatedButton(
-                      onPressed: () => Navigator.of(context).pop(color.toColor()),
-                      child: Text("Got it!")
-                    )
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         )
       )
     );
   }
 }
 
-Future<Color?> showColorPickerPage(BuildContext context) {
-  return Navigator.of(context).push(
-    PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => const ColorPickerPage(),
-      opaque: false,
-    )
-  );
-}
